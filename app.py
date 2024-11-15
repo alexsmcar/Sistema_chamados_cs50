eb9e3e97a064b0779daa3716ca42e08caff460e8
alexmcar <alexsandro.carvalho821@gmail.com>
2024-11-15 12:16:07 -0300

    inicio backend

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/dashboard')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template("dashboard.html")
+
+@app.route("/cadastrar")
+def cadastrar():
+    return render_template("cadastrar.html")
+
+@app.route("/chamados")
+def chamados():
+    return render_template("chamados.html")
+
+@app.route("/clientes")
+def clientes():
+    return render_template("clientes.html")
+
+
+
+
 
